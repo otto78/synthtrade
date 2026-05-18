c4a6a5b1f3f4d06e2670374b44c583d28e2f5c17
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2026-05-18 15:00:17 +0200

    test: add tests for TASK-209 (TDD Red phase)

---
+++ tests/test_task_209.py
@@ -0,0 +1,7 @@
+"""
+Tests for TASK-209: Refactor: RiskConfig dataclass iniettabile nei test
+"""
+
+def test_task_209_placeholder():
+    """Placeholder test - replace with actual tests."""
+    assert False, "Write actual tests here"

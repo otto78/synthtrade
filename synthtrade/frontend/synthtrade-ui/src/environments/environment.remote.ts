168343dcf18a0265b03b98187cc65e390dc682f8
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2026-08-06 12:50:01 +0200

    feat: local dev config pointing to render backend

---
+++ synthtrade/frontend/synthtrade-ui/src/environments/environment.remote.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://synthtrade-01f3.onrender.com/api',
+  wsUrl: 'wss://synthtrade-01f3.onrender.com/ws',
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+};

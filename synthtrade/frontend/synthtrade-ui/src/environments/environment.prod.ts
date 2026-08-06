651d3b5dfbfc7fa7d17ce4c00ec6ba3446e945ff
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2026-08-06 09:59:56 +0200

    deploy initail config

--- synthtrade/frontend/synthtrade-ui/src/environments/environment.prod.ts
+++ synthtrade/frontend/synthtrade-ui/src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  // TODO: Aggiornare con URL reale del backend Render dopo deploy
   apiUrl: 'https://synthtrade.onrender.com/api',
   wsUrl: 'wss://synthtrade.onrender.com/ws',
-  supabaseUrl: '', // TODO: Aggiungere URL Supabase
-  supabaseAnonKey: '', // TODO: Aggiungere Supabase Anon Key
+  supabaseUrl: '',
+  supabaseAnonKey: '',
 };
